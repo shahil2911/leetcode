f4b5fc5ae3ba86f50b6e21f266494853e1394daf
shahil2911 <77985549+shahil2911@users.noreply.github.com>
2023-12-21 22:55:14 +0530

    Time: 19 ms (92.88%) | Memory: 29 MB (84.85%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,17 +1,43 @@
+// class Solution {
+// public:
+//     int numSubarraysWithSum(vector<int>& nums, int goal) {
+//         unordered_map<int,int>mp;
+//         mp[0]=1;
+//         int pS=0, count=0;
+        
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             pS+=nums[i];
+//             if(mp[pS-goal])
+//                 count+=mp[pS-goal];
+//             mp[pS]++;
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int,int>mp;
-        mp[0]=1;
-        int pS=0, count=0;
+    int solverAtMost(vector<int>& nums, int goal) {
+        int left = 0, right = 0;
+        int n = nums.size(), sum = 0, atMost = 0;
         
-        for(int i=0;i<nums.size();i++)
-        {
-            pS+=nums[i];
-            if(mp[pS-goal])
-                count+=mp[pS-goal];
-            mp[pS]++;
+        if (goal < 0)
+            return 0;
+
+        while (right < n) {
+            sum += nums[right];
+            while (sum > goal) {
+                sum -= nums[left];
+                left++;
+            }
+            atMost += right - left + 1;
+            right++;
         }
-        return count;
+        return atMost;
+    }
+
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return solverAtMost(nums, goal) - solverAtMost(nums, goal - 1);
     }
 };
