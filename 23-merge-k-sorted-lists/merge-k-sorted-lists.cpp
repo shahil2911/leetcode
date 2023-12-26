f962ba1a14fb2f50e987a64fbc913d337e7d083c
shahil2911 <77985549+shahil2911@users.noreply.github.com>
2023-12-26 12:12:38 +0530

    Time: 12 ms (87.39%) | Memory: 13.5 MB (88.00%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -8,24 +8,55 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// class Solution {
+// public:
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         priority_queue<int,vector<int>, greater<int>> pq;
+//         for(int i=0;i<lists.size();i++){
+//             ListNode* current = lists[i];
+//             while (current != nullptr) {
+//                 pq.push(current->val);
+//                 current = current->next;
+//             }
+//         }
+//         ListNode* head=new ListNode(-1);
+//         ListNode* tail=head;
+//         while(!pq.empty()){
+//             tail->next=new ListNode(pq.top());
+//             pq.pop();
+//             tail=tail->next; 
+//         }
+//         return head->next; 
+//     }
+// };
+
+
+class myCompare{
+    public:
+    bool operator()(const ListNode* a, const ListNode* b){
+        return (a->val>b->val);
+    }
+};
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<int,vector<int>, greater<int>> pq;
-        for(int i=0;i<lists.size();i++){
-            ListNode* current = lists[i];
-            while (current != nullptr) {
-                pq.push(current->val);
-                current = current->next;
-            }
-        }
-        ListNode* head=new ListNode(-1);
+        ListNode* head= new ListNode(-1);
         ListNode* tail=head;
-        while(!pq.empty()){
-            tail->next=new ListNode(pq.top());
-            pq.pop();
-            tail=tail->next; 
+        priority_queue<ListNode*, vector<ListNode*>, myCompare>pq;
+        for(int i=0;i<lists.size();i++){
+            if(lists[i]!=nullptr)
+                pq.push(lists[i]);
         }
-        return head->next; 
+        while(!pq.empty()){
+            auto mini=pq.top();
+            pq.pop();
+            tail->next=mini;
+            tail=tail->next;
+            if(mini->next)
+                pq.push(mini->next);
+        }
+        return head->next;
+
     }
 };
